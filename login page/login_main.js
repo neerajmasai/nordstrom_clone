e1d07c4618469ffb4d7968d450b5ce9145ffb80f
Sumit <sanjay.sumit10@gmail.com>
2021-07-25 16:12:12 +0530

    little changes in navbar

--- login page/login_main.js
+++ login page/login_main.js
@@ -64,4 +64,36 @@ function home() {
     window.location.href = "../landing_page/landingPage.html";
     
 }
-
+window.onload = function logged() {
+    var isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
+    if(isLoggedIn){
+        //show name and logout option
+        var sign = document.getElementById("signInOption");
+        var currentEmail = JSON.parse(localStorage.getItem("currentEmail"));
+        var allEmails = JSON.parse(localStorage.getItem("allEmails"));
+        var details = allEmails.filter(function (el) {
+            return el.curEmail == currentEmail;
+        })[0];
+        sign.innerHTML = details.fName;
+        var logoutDiv = document.getElementById("signIn")
+        var logout = document.createElement("option");
+        logout.value = "Sign Out";
+        logout.innerHTML = "Sign Out";
+        logout.addEventListener("click", () => {
+            isLoggedIn = false;
+            localStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn));
+            window.location.reload();
+        });
+        logoutDiv.append(logout);
+    }
+    else{
+        //show Sign In
+        var sign = document.getElementById("signInOption");
+        sign.innerHTML = "Sign In";
+        sign.value = "Sign In";
+    }
+}
+function signIn(){
+    /* redirect to sign in page */
+    window.location.href = "../login page/login.html";
+}
